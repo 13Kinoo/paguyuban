c7035b40ef7d6832e3aceda14b06e63c7cec3f34
Yosefh-221305 <yosefh221305@gmail.com>
2024-11-28 22:40:39 +0700

    tambahan home

---
+++ application/views/Home/index.php
@@ -0,0 +1,124 @@
+<html>
+ <head>
+  <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" rel="stylesheet"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+  <style>
+   body {
+            background: url('assets/WhatsApp Image 2024-11-28 at 20.45.30_85828e7c.jpg') no-repeat center center fixed;
+            background-size: cover;
+            color: white;
+            font-family: Arial, sans-serif;
+        }
+        .navbar {
+            background-color: rgba(255, 255, 255, 0.3);
+            padding: 0.5rem 1rem; /* Mengurangi padding navbar */
+            border: none; /* Menghilangkan border navbar */
+        }
+        .navbar-nav .nav-link {
+            color: white !important;
+            margin-left: 15px; /* Menambahkan jarak antar link */
+        }
+        .navbar-brand img {
+            height: 50px; /* Ukuran logo */
+            width: auto; /* Memastikan lebar otomatis */
+        }
+        .content {
+            text-align: center;
+            margin-top: 100px;
+        }
+        .content h1 {
+            font-size: 48px;
+            font-weight: bold;
+        }
+        .icon-container {
+            margin-top: 50px;
+        }
+        .icon-container .icon-box {
+            display: inline-block;
+            width: 150px;
+            height: 150px;
+            background-color: rgba(255, 255, 255, 0.7);
+            border-radius: 10px;
+            margin: 10px;
+            text-align: center;
+            padding: 20px;
+        }
+        .icon-container .icon-box i {
+            font-size: 48px;
+            margin-bottom: 10px;
+        }
+        .icon-container .icon-box p {
+            font-size: 18px;
+            font-weight: bold;
+            margin: 0;
+        }
+  </style>
+ </head>
+ <body>
+  <nav class="navbar navbar-expand-lg">
+   <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+     <img alt="Logo" src="assets/Rectangle 23.png"/>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+     <ul class="navbar-nav ms-auto">
+      <li class="nav-item">
+       <a class="nav-link" href="#">
+        Home
+       </a>
+      </li>
+      <li class="nav-item">
+       <a class="nav-link" href="#">
+        Tentang Kami
+       </a>
+      </li>
+      <li class="nav-item">
+       <a class="nav-link" href="#">
+        Akun
+       </a>
+      </li>
+      <li class="nav-item">
+       <a class="nav-link" href="#">
+        Ganti password
+       </a>
+      </li>
+     </ul>
+    </div>
+   </div>
+  </nav>
+  <div class="content">
+   <h1>
+    Bersama dalam Kepedulian, Berbagi dalam Kehilangan
+   </h1>
+   <div class="icon-container">
+    <div class="icon-box">
+     <i class="fas fa-users "></i>
+     <p>
+      Anggota
+     </p>
+    </div>
+    <div class="icon-box">
+     <i class="fas fa-dollar-sign"></i>
+     <p>
+      Iuran
+     </p>
+    </div>
+    <div class="icon-box">
+     <i class="fas fa-hand-holding-heart"></i>
+     <p>
+      Santunan
+     </p>
+    </div>
+    <div class="icon-box">
+     <i class="fas fa-coins"></i>
+     <p>
+      Keuangan
+     </p>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
